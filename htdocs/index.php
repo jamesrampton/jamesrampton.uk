2c97f064e1ed13b6dc1ff99493504ec4a9bb5f27
James Rampton <me@jamesrampton.com>
2020-11-16 17:15:24 +0000

    Add social icons

--- htdocs/index.php
+++ htdocs/index.php
@@ -5,6 +5,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0 user-scalable=no">
     <link rel="shortcut icon" type="image/png" href="/img/favicon.png" sizes="32x32">
     <link href="https://fonts.googleapis.com/css2?family=Darker+Grotesque&display=swap" rel="stylesheet">
+    <script src="https://kit.fontawesome.com/ea942b1dc0.js" crossorigin="anonymous"></script>
     <title>James Rampton</title>
 </head>
 <body>
@@ -35,19 +36,34 @@
         font-family: 'Darker Grotesque', sans-serif;
         font-size: 100%;
     }
-    #logo {
+    #wrapper {
         position: absolute;
-        top: 50%;
+        top: calc(50% - 90px);
         left: 50%;
         z-index: 10;
         width: 300px;
         height: 300px;
         margin: -150px 0 0 -150px;
-        fill: #292929;
+        fill: #333333;
         //-webkit-animation: pulse 3s 1;
         //-moz-animation: pulse 3s 1;
         //-o-animation: pulse 3s 1;
         //animation: pulse 3s 1;
+    }
+    #logo {
+    	width: 300px;
+	height: 300px;
+    }
+    #social {
+	text-align: center;
+	margin-top: 50px;
+	font-size: 40px;
+    }
+    .social-link, .social-link:visited {
+	padding: 15px;
+	color: #333333;
+	text-decoration: none;
+	display: inine-block;
     }
     #copyright {
         position: absolute;
@@ -57,7 +73,14 @@
         color: #bbbbbb;
     }
 </style>
+<div id="wrapper">
 <svg id="logo" xmlns="http://www.w3.org/2000/svg" width="642" height="642" viewBox="0 0 642 642"><path d="M342.41,235.76H410C410,451.7,226,628.35.52,628.35V569.46c170.13,0,313.49-117.76,342.6-275h-29.6a319.77,319.77,0,0,0-20.07,111.76H231.94c0-215.94,184-392.59,409.54-392.59V72.54C515.5,72.54,403.53,137.71,342.41,235.76Z"/></svg>
+<div id="social">
+<a class="social-link" href="https://vimeo.com/jamesrampton"><i class="fa fa-vimeo"></i></a>
+<a class="social-link" href="https://soundcloud.com/jamesrampton"><i class="fa fa-soundcloud"></i></a>
+<a class="social-link" href="https://jamesrampton.bandcamp.com"><i class="fa fa-bandcamp"></i></a>
+</div>
+</div>
 <p id="copyright">&copy; James Rampton <?php echo date("Y"); ?></p>
 </body>
 </html>
