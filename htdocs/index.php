e470d8de3745510c2c8eba5ab49300ab7c6f6153
James Rampton <me@jamesrampton.com>
2020-12-03 08:32:11 +0000

    Revert "Add gauges tracking code"
    This reverts commit e352c6bf3a0c7bb73e2d01c755bf7d480e4de278.

--- htdocs/index.php
+++ htdocs/index.php
@@ -62,19 +62,5 @@
 </div>
 </div>
 <p id="copyright">&copy; James Rampton <?php echo date("Y"); ?></p>
-<script type="text/javascript">
-  var _gauges = _gauges || [];
-  (function() {
-    var t   = document.createElement('script');
-    t.type  = 'text/javascript';
-    t.async = true;
-    t.id    = 'gauges-tracker';
-    t.setAttribute('data-site-id', '5fb5611c7f48dd2c24375a50');
-    t.setAttribute('data-track-path', 'https://track.gaug.es/track.gif');
-    t.src = 'https://d2fuc4clr7gvcn.cloudfront.net/track.js';
-    var s = document.getElementsByTagName('script')[0];
-    s.parentNode.insertBefore(t, s);
-  })();
-</script>
 </body>
 </html>
