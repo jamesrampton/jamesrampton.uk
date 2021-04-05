055e4a50455911e44c65e7b11846c3818693df46
James Rampton <me@jamesrampton.com>
2021-04-05 18:11:57 +0100

    Fix IDE warnings

--- publishconf.py
+++ publishconf.py
@@ -7,7 +7,7 @@
 import os
 import sys
 sys.path.append(os.curdir)
-from pelicanconf import *
+from pelicanconf import *  # noqa
 
 # If your site is available via HTTPS, make sure SITEURL begins with https://
 SITEURL = 'https://www.jamesrampton.uk'
